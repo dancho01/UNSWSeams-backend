43916b138fa9a043498afc98418f6663ce19a643
Yuzhi Zhao <z5308264@ad.unsw.edu.au>
2022-03-01 21:17:47 +1100

    fixed issues due to the migration of functions to data_store.py

--- tests/other_test.py
+++ tests/other_test.py
@@ -1,5 +1,7 @@
 import pytest
 from src.other import clear_v1
+from src.auth import auth_register_v1
+from src.channels import channels_create_v1
 
 
 
@@ -31,4 +33,10 @@ def test_clear_v1():
     }
 
     assert(clear_v1() == {})
-    
+
+
+def test1_clear_v1():
+    clear_v1()                                                  #Ensures that the datastruct is completely empty
+    auth_register_v1('random123@gmail.com', '1234', 'Bob', 'James')
+    channels_create_v1('1', 'Bob', True)
+    clear_v1()
