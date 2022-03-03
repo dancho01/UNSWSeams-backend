73f100fd1f72e8828aac256d1a33f17c393faa18
Yuzhi Zhao <z5308264@ad.unsw.edu.au>
2022-03-03 23:29:13 +1100

    update to move to ssh

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -1,47 +1,22 @@
 import pytest
-from src.channel import channel_messages_v1
 from src.error import InputError, AccessError
+from src.channel import channel_messages_v1, channel_details_v1
+from src.channels import channels_create_v1
+from src.other import clear_v1
+from src.auth import auth_register_v1, auth_login_v1
 
-# Test for invalid start                        #assuming auth_id = 5 : valid, user_id = 6 : valid, start >= 0 : valid
+# tests for channel_details_v1
 
 
-def test_0_invalid_start():
-    # assuming auth_id: valid, user_id: valid, start: invalid
+def test_invalid_channel():
+    clear_v1()
+    auth_register_v1("bob.smith@gmail.com", "comp1531", "Bob", "Smith")
+    first_auth_id = int(auth_login_v1(
+        "bob.smith@gmail.com", "comp1531").get("auth_user_id"))
+    valid_channel_id = int(channels_create_v1(
+        first_auth_id, "first_channel", True)["channel_id"])
     with pytest.raises(InputError):
-        channel_messages_v1(5, 6, -1)
+        channel_details_v1(first_auth_id, valid_channel_id + 1)
 
 
-def test_1_invalid_start():
-    # assuming auth_id: invalid, user_id: valid, start: invalid
-    with pytest.raises(InputError):
-        channel_messages_v1(4, 6, -100)
-
-
-def test_2_invalid_start():
-    # assuming auth_id: valid, user_id: invalid, start: invalid
-    with pytest.raises(InputError):
-        channel_messages_v1(4, 5, -32)
-
-
-def test_3_invalid_start():
-    # assuming auth_id: invalid, user_id: invalid, start: invalid
-    with pytest.raises(InputError):
-        channel_messages_v1(3, 2, 24)
-
-
-def test_4_invalid_channel():
-    # assuming auth_id: invalid, user_id: valid, start: valid
-    with pytest.raises(InputError):
-        channel_messages_v1(4, 6, 3)
-
-
-def test5_invalid_channel():
-    # assuming auth_id: invalid, user_id: invalid, start: valid
-    with pytest.raises(InputError):
-        channel_messages_v1(7, 23, 3)
-
-
-def test_6_invalid_authorization():
-    # assuming auth_id: valid, user_id: invalid, start: valid
-    with pytest.raises(AccessError):
-        channel_messages_v1(5, 14, 3)
+# tests for channel_messages_v1
