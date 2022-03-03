374f521fdbc2eba838d250ec4a3be9d0c2609ffa
Yuzhi Zhao <z5308264@ad.unsw.edu.au>
2022-03-04 00:22:56 +1100

    Added test cases

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -19,4 +19,21 @@ def test_invalid_channel():
         channel_details_v1(first_auth_id, valid_channel_id + 1)
 
 
+def test_invalid_user():
+    clear_v1()
+    auth_register_v1("bob.smith@gmail.com", "comp1531", "Bob", "Smith")
+    first_auth_id = int(auth_login_v1(
+        "bob.smith@gmail.com", "comp1531").get("auth_user_id"))
+    valid_channel_id = int(channels_create_v1(
+        first_auth_id, "first_channel", True)["channel_id"])
+    with pytest.raises(AccessError):
+        channel_details_v1(first_auth_id + 1, valid_channel_id)
+
+
 # tests for channel_messages_v1
+
+def invalid_start_index():
+    clear_v1()
+    auth_register_v1("bob.smith@gmail.com", "comp1531", "Bob", "Smith")
+    first_auth_id = int(auth_login_v1(
+        "bob.jane@gmail.com", "comp1531").get("auth_user_id"))
