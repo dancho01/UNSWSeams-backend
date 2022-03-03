a3124dce0759f9bf6d5ee9362f385d4b0b0eb2e1
Yuzhi Zhao <z5308264@ad.unsw.edu.au>
2022-03-04 01:24:02 +1100

    done

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -4,7 +4,6 @@ from src.channel import channel_messages_v1, channel_details_v1
 from src.channels import channels_create_v1
 from src.other import clear_v1
 from src.auth import auth_register_v1, auth_login_v1
-from src.data_store import data_store
 
 # tests for channel_details_v1
 
