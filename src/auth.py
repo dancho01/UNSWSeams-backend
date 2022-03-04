fea6857d0ba77dafa4d73c819255ca3f2c052374
Daniel Cho <z5311680@ad.unsw.edu.au>
2022-03-05 00:01:32 +1100

    made some changes to auth

--- src/auth.py
+++ src/auth.py
@@ -6,9 +6,6 @@ from src.error import InputError
 def auth_login_v1(email, password):
     store = data_store.get()
 
-    if len(email) < 1:
-        raise InputError("Enter an email!")
-
     found = False
     for user in store['users']:
         if user['email'] == email:
@@ -16,15 +13,6 @@ def auth_login_v1(email, password):
 
     if found != True:
         raise InputError("This email is not registered!")
-
-    if '.' not in email:
-        raise InputError("No dot in email!")
-
-    if '@' not in email:
-        raise InputError("No @ in email!")
-
-    if not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
-        raise InputError("Invalid email!")
 
     for user in store['users']:
         if user['email'] == email:
@@ -60,17 +48,11 @@ def auth_register_v1(email, password, name_first, name_last):
         raise InputError(
             "Input a last name less than or equal to 50 characters!")
 
-    if '.' not in email:
-        raise InputError("No dot in email!")
-
-    if '@' not in email:
-        raise InputError("No @ in email!")
-
-    if len(email) < 1:
-        raise InputError("Input an email!")
-
     if len(password) < 6:
         raise InputError("Password must be 6 or more characters!")
+        
+    if not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
+        raise InputError("Invalid email!")
 
     for user in store['users']:
         if user['email'] == email:
