cf323b6f9ecf4e903a7e15ca55b0c21a0cb08bef
Yuzhi Zhao <z5308264@ad.unsw.edu.au>
2022-03-05 00:03:31 +1100

    fixed import error

--- tests/test_channel_list.py
+++ tests/test_channel_list.py
@@ -8,14 +8,18 @@ from src.auth import auth_register_v1
 from src.channels import channels_create_v1
 from src.other import clear_v1
 from src.data_store import check_valid_channel, check_authorization, messages_returned, data_store, check_user_registered
-from test_channel_invite import test_invalid_auth_user_id
+
 
 def test_invalid_auth_user_id():
     # when auth_user_id does not exist
     clear_v1()
-    u_id2 = int(auth_register_v1("another_email@domain.com", "Password2", "First", "Last")["auth_user_id"])
-    u_id3 = int(auth_register_v1("third_email@domain.com", "Password3", "First", "Last")["auth_user_id"])
-    channel1 = int(channels_create_v1(u_id2, "Channel Name", True)["channel_id"])   # returns an integer channel_id
+    u_id2 = int(auth_register_v1("another_email@domain.com",
+                "Password2", "First", "Last")["auth_user_id"])
+    u_id3 = int(auth_register_v1("third_email@domain.com",
+                "Password3", "First", "Last")["auth_user_id"])
+    channel1 = int(channels_create_v1(u_id2, "Channel Name", True)[
+                   "channel_id"])   # returns an integer channel_id
 
     with pytest.raises(AccessError):
-        channel_invite_v1(1, channel1, u_id3)   # assumes 1 is not a valid user_id
+        # assumes 1 is not a valid user_id
+        channel_invite_v1(1, channel1, u_id3)
