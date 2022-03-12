830b950d6fd89027ac7b460e281d2e6c01f59be6
Yuzhi Zhao <z5308264@ad.unsw.edu.au>
2022-03-12 17:54:08 +1100

    fixed servers.py

--- src/server.py
+++ src/server.py
@@ -6,9 +6,11 @@ from flask_cors import CORS
 from src.error import InputError
 from src import config
 
+
 def quit_gracefully(*args):
     '''For coverage'''
     exit(0)
+
 
 def defaultHandler(err):
     response = err.get_response()
@@ -21,26 +23,30 @@ def defaultHandler(err):
     response.content_type = 'application/json'
     return response
 
+
 APP = Flask(__name__)
 CORS(APP)
 
 APP.config['TRAP_HTTP_EXCEPTIONS'] = True
 APP.register_error_handler(Exception, defaultHandler)
 
-#### NO NEED TO MODIFY ABOVE THIS POINT, EXCEPT IMPORTS
+# NO NEED TO MODIFY ABOVE THIS POINT, EXCEPT IMPORTS
 
 # Example
+
+
 @APP.route("/echo", methods=['GET'])
 def echo():
     data = request.args.get('data')
     if data == 'echo':
-   	    raise InputError(description='Cannot echo "echo"')
+        raise InputError(description='Cannot echo "echo"')
     return dumps({
         'data': data
     })
 
-#### NO NEED TO MODIFY BELOW THIS POINT
+# NO NEED TO MODIFY BELOW THIS POINT
+
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, quit_gracefully) # For coverage
-    APP.run(port=config.port) # Do not edit this port
+    signal.signal(signal.SIGINT, quit_gracefully)  # For coverage
+    APP.run(port=config.port)  # Do not edit this port
