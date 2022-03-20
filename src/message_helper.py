ad34f6e8b20cd02cdfc33522bfa18d7a38d4aca1
Daniel Cho <z5311680@ad.unsw.edu.au>
2022-03-20 17:32:26 +1100

    Made a file for helper functions for message related functions

---
+++ src/message_helper.py
@@ -0,0 +1,15 @@
+
+MESSAGE_ID_COUNTER = 0
+    
+def generate_new_message_id():
+    '''
+    Generates a new message_id that is unique and sequentially increases by 1
+    Args:
+        None
+    Return:
+        Returns the next message_id
+    ''' 
+    global MESSAGE_ID_COUNTER
+    MESSAGE_ID_COUNTER += 1
+    return MESSAGE_ID_COUNTER
+    
