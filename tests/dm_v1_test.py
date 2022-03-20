26051cac7496e1666eff8349534c35a78d9fcf7c
Daniel Cho <z5311680@ad.unsw.edu.au>
2022-03-20 17:29:39 +1100

    Wrote additional tests for dm feature

--- tests/dm_v1_test.py
+++ tests/dm_v1_test.py
@@ -83,11 +83,11 @@ def test_dm_remove_invalid_dm():
 def test_dm_remove_not_owner():
     requests.delete(config.url + 'clear/v1')
     
-    user1 = requests.post(config.url + 'auth_register/v2', json = {'email' : 'firstuser@gmail.com', 
+    user1 = requests.post(config.url + 'auth/register/v2', json = {'email' : 'firstuser@gmail.com', 
     'password' : 'password', 'name_first' : 'First', 'name_last' : 'Last'}) 
-    user1_data = user.json()
+    user1_data = user1.json()
     
-    user2 = requests.post(config.url + 'auth_register/v2', json = {'email' :  'seconduser@gmail.com', 
+    user2 = requests.post(config.url + 'auth/register/v2', json = {'email' :  'seconduser@gmail.com', 
     'password' : 'password', 'name_first' : 'DifferentFirst', 'name_last' : 'DifferentLast'}) 
     user2_data = user2.json()
    
@@ -99,18 +99,17 @@ def test_dm_remove_not_owner():
     assert dm_remove.status_code == 403
     
     
-def test_dm_remove_user_already_removed():
+def test_dm_remove_owner_already_removed():
     requests.delete(config.url + 'clear/v1')
     
-    user1 = requests.post(config.url + 'auth_register/v2', json = {'email' : 'firstuser@gmail.com', 
+    user1 = requests.post(config.url + 'auth/register/v2', json = {'email' : 'firstuser@gmail.com', 
     'password' : 'password', 'name_first' : 'First', 'name_last' : 'Last'}) 
-    user1_data = user.json()
+    user1_data = user1.json()
    
     dm_response = requests.post(config.url + 'dm/create/v1', json = {'token': user1_data['token'] , 'u_ids': []}) 
     dm_data = dm_response.json()    
     
-    dm_remove = requests.delete(config.url + 'dm/remove/v1', json = {'token': user1_data['token'] , 'dm_id': dm_data['dm_id']})
-    assert dm_remove.status_code == 200
+    dm_remove = requests.post(config.url + 'dm/leave/v1', json = {'token': user1_data['token'] , 'dm_id': dm_data['dm_id']})
    
     dm_remove = requests.delete(config.url + 'dm/remove/v1', json = {'token': user1_data['token'] , 'dm_id': dm_data['dm_id']})
     assert dm_remove.status_code == 403    
@@ -149,8 +148,23 @@ def test_dm_details_user_not_member():
     dm_details = requests.get(config.url + 'dm/details/v1', params = {'token': user2_data['token'] , 'dm_id': dm_data['dm_id']})
     
     assert dm_details.status_code == 403
-   
     
+    
+def test_dm_details_success():
+    requests.delete(config.url + 'clear/v1')   
+     
+    user1 = requests.post(config.url + 'auth/register/v2', json = {'email' : 'firstuser@gmail.com', 
+    'password' : 'password', 'name_first' : 'First', 'name_last' : 'Last'}) 
+    user1_data = user1.json()    
+    
+    dm_response = requests.post(config.url + 'dm/create/v1', json = {'token': user1_data['token'] , 'u_ids': []}) 
+    dm_data = dm_response.json()
+    
+    dm_details = requests.get(config.url + 'dm/details/v1', params = {'token': user1_data['token'] , 'dm_id': dm_data['dm_id']})
+   
+    assert dm_details.status_code == 200
+   
+       
 ''' tests for dm/leave/v1 '''
 def test_dm_leave_invalid_dm():
     requests.delete(config.url + 'clear/v1')
@@ -244,7 +258,7 @@ def test_dm_send_message_user_not_member():
     
     user2 = requests.post(config.url + 'auth/register/v2', json = {'email' : 'seconduser@gmail.com', 
     'password' : 'Differentpassword', 'name_first' : 'DifferentFirst', 'name_last' : 'DifferentLast'}) 
-    user2_data = user1.json()
+    user2_data = user2.json()
     
     dm_response = requests.post(config.url + 'dm/create/v1', json = {'token': user1_data['token'] , 'u_ids': []}) 
     dm_data = dm_response.json()
@@ -253,9 +267,24 @@ def test_dm_send_message_user_not_member():
     
     assert dm_sent.status_code == 403     
     
+
+def test_dm_send_message_success():
+    requests.delete(config.url + 'clear/v1')
     
+    user1 = requests.post(config.url + 'auth/register/v2', json = {'email' : 'firstuser@gmail.com', 
+    'password' : 'password', 'name_first' : 'First', 'name_last' : 'Last'}) 
+    user1_data = user1.json()
+    
+    user2 = requests.post(config.url + 'auth/register/v2', json = {'email' : 'seconduser@gmail.com', 
+    'password' : 'Differentpassword', 'name_first' : 'DifferentFirst', 'name_last' : 'DifferentLast'}) 
+    user2_data = user2.json()
+    
+    dm_response = requests.post(config.url + 'dm/create/v1', json = {'token': user1_data['token'] , 'u_ids': []}) 
+    dm_data = dm_response.json()
+    
+    dm_sent = requests.post(config.url + 'message/senddm/v1', json = {'token': user1_data['token'] , 'dm_id': dm_data['dm_id'], 'message': 'this is a message'})    
 
-
+    assert dm_sent.status_code == 200
 
 
 
