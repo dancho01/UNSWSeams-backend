337f69ae70417fc8ca306c2b1f115648ca67c7e4
z5319978 <z5319978@cse.unsw.edu.au>
2022-03-21 19:34:40 +1100

    changed initial functions to take in tokens and decode them

--- src/channels.py
+++ src/channels.py
@@ -1,8 +1,9 @@
 from src.data_store import data_store, check_user_registered, return_member_information
 from src.error import InputError, AccessError
+from src.token import check_valid_token
 
 
-def channels_list_v1(auth_user_id):
+def channels_list_v1(token):
     '''
     Provide a list of all channels (and their associated details) that the authorised user is part of.
 
@@ -18,8 +19,8 @@ def channels_list_v1(auth_user_id):
     '''
     store = data_store.get()
 
-    if check_user_registered(auth_user_id, store) == False:
-        raise AccessError('auth_user_id passed in is invalid')
+    user_data = check_valid_token(token)
+    auth_user_id = user_data['u_id']
 
     channel_return = []
     for channel in store['channels']:
@@ -31,7 +32,7 @@ def channels_list_v1(auth_user_id):
     return {'channels': channel_return}
 
 
-def channels_listall_v1(auth_user_id):
+def channels_listall_v1(token):
     '''
     Provide a list of all channels (and their associated details) that the authorised user is part of.
 
@@ -47,6 +48,9 @@ def channels_listall_v1(auth_user_id):
     '''
     store = data_store.get()
 
+    user_data = check_valid_token(token)
+    auth_user_id = user_data['u_id']
+
     if check_user_registered(auth_user_id, store) == False:
         raise AccessError('auth_user_id passed in is invalid')
 
@@ -58,7 +62,7 @@ def channels_listall_v1(auth_user_id):
     return {'channels': channel_return}
 
 
-def channels_create_v1(auth_user_id, name, is_public):
+def channels_create_v1(token, name, is_public):
     '''
     This function allows an authorized user to request to make a new channel
     with the given name that is either a public or private channel. The user
@@ -78,6 +82,9 @@ def channels_create_v1(auth_user_id, name, is_public):
         successfully created
     '''
     store = data_store.get()
+
+    user_data = check_valid_token(token)
+    auth_user_id = user_data['u_id']
 
     if check_user_registered(auth_user_id, store) == False:
         raise AccessError('auth_user_id passed in is invalid')
