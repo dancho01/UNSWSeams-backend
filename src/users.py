3f4b016a7ad44e7ecc601fefe29b9d3ded06140b
Yuzhi Zhao <z5308264@ad.unsw.edu.au>
2022-03-23 23:50:42 +1100

    bug fixes and temporarily disabled some tests due to fixes

--- src/users.py
+++ src/users.py
@@ -1,13 +1,8 @@
-from src.error import InputError, AccessError
-from src.data_store import data_store, return_member_information, check_user_registered
-from datetime import timezone
 from src.token import check_valid_token
 from src.users_helpers import return_users_information
-import datetime
 
 
 def users_all_v1(token):
-    store = data_store.get()
 
     check_valid_token(token)
 
