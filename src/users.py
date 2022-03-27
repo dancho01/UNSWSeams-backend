a287efb5ec85d136fb633cb018bf005a7f57bbf8
z5319978 <z5319978@cse.unsw.edu.au>
2022-03-27 21:22:48 +1100

    made more helper functions and added docstrings for relevant functions

--- src/users.py
+++ src/users.py
@@ -3,6 +3,14 @@ from src.users_helpers import return_users_information
 
 
 def users_all_v1(token):
+    '''
+    Returns a list of all users and their associated details.
+    Args:
+        token           str         user's token     
+    Return Value:
+        Returns dictionary containing {users} dictioanry, containing a list of dictionaries contains
+        {auth_user_id, email, first name, last name, and handle}
+    '''
 
     check_valid_token(token)
 
