5c899eb7ba5b73e21bbf2e9ab68fcfa6085615df
Daniel Cho <z5311680@ad.unsw.edu.au>
2022-04-10 12:13:47 +1000

    Wrote up implementation for react/unreact

--- src/iter3_message_helper.py
+++ src/iter3_message_helper.py
@@ -1,3 +1,4 @@
+from src.error import AccessError, InputError
 
 def is_user_joined(channel_dm, u_id):
     for member in channel_dm['all_members']:
@@ -5,9 +6,67 @@ def is_user_joined(channel_dm, u_id):
             return True
     return False
     
+    
 def return_messages_with_query(query_str, channel_dm, query_collection):
-
     for message in channel_dm['messages']:
         if query_str.lower() in message['message'].lower():
             query_collection.append(message)
     return query_collection
+    
+    
+def check_invalid_message_id(store, message_id, user_id):
+    for channel in store['channels']:
+        if is_user_joined(channel, user_id):
+            result = is_message_id_valid(channel, message_id)
+            if result != False:            
+                return result
+      
+    for dm in store['dms']:
+        if is_user_joined(dm, user_id):
+            result = is_message_id_valid(dm, message_id)
+            if result != False:
+                return result
+        
+    raise InputError(description='message id is not valid')                  
+      
+                   
+def is_message_id_valid(channel_dm, message_id):
+    for message in channel_dm['messages']:
+        if message['message_id'] == message_id:
+            return message
+    return False
+    
+def has_user_already_reacted(message, user_id, react_id):
+    for react in message['reacts']:
+        if react['react_id'] == react_id:
+            for u_id in react['u_ids']:
+                if u_id == user_id:
+                    return True
+    return False
+    
+    
+def add_react_message(user_id, message, react_id):
+    for react in message['reacts']:
+        if react['react_id'] == react_id:
+            react['u_ids'].append = user_id
+            return
+    
+    new_react = generate_new_react(user_id, react_id)
+    
+    message['reacts'].append(new_react)
+    
+def remove_react_message(user_id, message, react_id):
+    for react in message['reacts']:
+        if react['react_id'] == react_id:
+            react['u_ids'] = list(filter(lambda i: i != user_id, react['u_ids']))
+              
+    
+def generate_new_react(user_id, react_id):
+    new_react = {
+        'react_id': react_id,
+        'u_ids': [],
+        'is_this_user_reacted': True 
+    }    
+    new_react['u_ids'].append(user_id)
+    
+    return new_react
--- src/message_iter3.py
+++ src/message_iter3.py
@@ -2,8 +2,7 @@ from src.data_store import data_store
 from src.error import InputError, AccessError
 from src.token import check_valid_token
 from src.channel_helper import check_message
-from src.iter3_message_helper import is_user_joined, return_messages_with_query
-import re
+from src.iter3_message_helper import is_user_joined, return_messages_with_query, is_message_id_valid, has_user_already_reacted, add_react_message, remove_react_message, check_invalid_message_id
 
 def search_v1(token, query_str):
     store = data_store.get()
@@ -25,3 +24,39 @@ def search_v1(token, query_str):
         'messages': query_collection
     }
     
+    
+def message_react_v1(token, message_id, react_id):
+    
+    store = data_store.get()
+    user_id = check_valid_token(token)['u_id']
+    
+    message = check_invalid_message_id(store, message_id, user_id)
+            
+    if react_id is not 1:
+        raise InputError(description='not a valid react ID') 
+           
+    if has_user_already_reacted(message, user_id, react_id):
+        raise InputError(description='user has already reacted to this message')
+    
+    add_react_message(user_id, message, react_id)
+
+    return {}
+    
+    
+    
+def message_unreact_v1(token, message_id, react_id):   
+    
+    store = data_store.get()
+    user_id = check_valid_token(token)['u_id']
+    
+    message = check_invalid_message_id(store, message_id, user_id)
+    
+    if react_id is not 1:
+        raise InputError(description='not a valid react ID') 
+    
+    if has_user_already_reacted(message, user_id, react_id) == False:
+        raise InputError(description='user has not reacted to this message')    
+    
+    remove_react_message(user_id, message, react_id)
+    
+    return {}
