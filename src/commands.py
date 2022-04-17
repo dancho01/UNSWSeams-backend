832419013c673e64907bb2ca6e38d83b66928397
Yuzhi Zhao <z5308264@ad.unsw.edu.au>
2022-04-18 01:01:48 +1000

    fixed tests

--- src/commands.py
+++ src/commands.py
@@ -84,15 +84,15 @@ def recognise_commands(token, channel_id, message):
 
     # /abot activates bot, /dbot deactivates bot
     channel_members = store['channels'][c_info['c_dex']]['all_members']
-
+    channel_owners = store['channels'][c_info['c_dex']]['owner_members']
     if command == "abot":
 
-        if len(channel_members) > 0:
+        if len(channel_owners) != 0:
             check_command_owner_status(c_info['c_dex'], c_id)
         store['channels'][c_info['c_dex']]['bot_status'] = True
         return True
     elif command == "dbot":
-        if len(channel_members) > 0:
+        if len(channel_owners) != 0:
             check_command_owner_status(c_info['c_dex'], c_id)
         store['channels'][c_info['c_dex']]['bot_status'] = False
         return True
@@ -114,7 +114,7 @@ def recognise_commands(token, channel_id, message):
         t.start()
         return True
     elif command == "clearchat":
-        if len(channel_members) != 0:
+        if len(channel_owners) != 0:
             check_command_owner_status(c_info['c_dex'], c_id)
         command_clear_chat(channel_id)
         return True
